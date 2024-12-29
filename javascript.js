c12550fb974a8aebc552df4f6e37ecf97012cc2b
DoMilovec <dmilovec@gmail.com>
2024-12-29 15:33:07 +0100

    Add logic for creating grid of 'n * n'
    n only changable in javascript file so far

--- javascript.js
+++ javascript.js
@@ -1,9 +1,13 @@
 let n = 16;
-const container = document.querySelector('#container');
+const container = document.querySelector('.container');
+let width = 512/n;
+let height = 512/n;
 
-for (let i = 0 ; i < n ; i++) {
+for (let i = 0 ; i < n * n ; i++) {
     const box = document.createElement('div');
     box.classList.add('box');
     container.appendChild(box);
-
+    box.style.border = '1px blue solid';
+    box.style.width = `${width}px`;
+    box.style.height = `${height}px`;
 }
